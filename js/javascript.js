7ac62b9d9344e0ac6b703272d54b08a224be3c13
Daniel Boisselle <dboisselle@hotmail.com>
2017-08-17 10:12:27 -0400

    modular code, fixed unnecessary statements, small bugs fixed

--- js/javascript.js
+++ js/javascript.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const cc_num = document.getElementById('cc-num');
 	const zip = document.getElementById('zip');
 	const cvv = document.getElementById('cvv');
-	const submitButton = document.querySelectorAll('button[type=submit]')[0];
+	const $submitButton = $("[type='submit']");;
 
 	const $colors_js_puns = $("#colors-js-puns");
 	const $other_title = $("#other-title");
@@ -30,33 +30,35 @@ document.addEventListener('DOMContentLoaded', () => {
 	const $paypalMessage = $("[id='paypal message']");
 	const $bitcoinMessage = $("[id='bitcoin message']");
 
-	document.getElementById('name').select();
-	//set the default value of the payment selector to 'credit card'
-	paymentMethod.value = "credit card";
-	$paypalMessage.hide();
-	$bitcoinMessage.hide();
+	var hideAndSetDefaults =(() => {
+		name.select();
+		$other_title.hide();
+		$colors_js_puns.hide();
+		paymentMethod.value = "credit card";
+		$paypalMessage.hide();
+		$bitcoinMessage.hide();
+	})();
 
-	//hide t-shirt design colors
-	//wait until shirt design is selected before displayed
-	$colors_js_puns.hide();
+	userTitle.addEventListener("change", showTitleIfOtherSelected);
+	tShirtDesignSelect.addEventListener("change", changeTShirts);
+	activities.addEventListener("change", checkboxCompatability);
+	paymentMethod.addEventListener("change", changePaymentMethod);
+	email.addEventListener("keyup", emailLiveValidCheck);
+	$submitButton.on('click', checkIfValidForm);
 
-	//no display until "other" job role is selected
-	$other_title.hide();
-	//Other job role open input when selected / deselected
-	userTitle.addEventListener("change", (e) => {
+	function showTitleIfOtherSelected(e) {
 		let selected = e.target.value;
-		selected == "other" ? $other_title.show() : $other_title.hide();;
-	});
+		selected == "other" ? $other_title.show() : $other_title.hide();
+	}
 
-	//T-shirt infor design/color match function
-	tShirtDesignSelect.addEventListener("change", (e) => {
+	function changeTShirts (e) {
 		let shirt = e.target.value;
 		//making the color input visibile for selection
 		$colors_js_puns.show();
 
 		let showShirts = (x) => {
 			$colors_js_puns.show();
-		 	$("#color").html(x);
+			$("#color").html(x);
 		};
 
 		if(shirt == 'js puns'){
@@ -70,32 +72,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		}else{
 			$colors_js_puns.hide();
 		}
-	});
+	}
 
 	let activitiesCostTotal = 0;
-	//function to enabled / disable checkboxes when times and days are contradicting
-	activities.addEventListener("change", (e) => {
-		//target checkbox
+	function checkboxCompatability (e) {
 		let checkboxChecked = e.target.checked;
-		//checkbox name
 		let checkboxName = e.target.name;
-
-		let checkBoxChangeColor = () => {
-			checkboxChecked ? e.target.parentNode.style.color = 'green' : e.target.parentNode.style.color = '#000';
-		}
-
-		let priceAdjustment = (price) => {
-			return checkboxChecked ? activitiesCostTotal += price : activitiesCostTotal -= price;
-		}
-
-		let enableBox = (i, price) => {
-			//terenary condition returning the contradicting checkbox, enabling it when the
-			//target id deselected or selected
-			priceAdjustment(price);
-			checkBoxChangeColor();
-			checkboxChecked ? checkbox[i].style.color = 'red' : checkbox[i].style.color = '';
-			return checkboxChecked ? checkbox[i].disabled = true : checkbox[i].disabled = false;
-		}
+		let parentLabelNode = e.target.parentNode;
 
 		if (checkboxName == 'js-frameworks') {
 			enableBox(3, 100);
@@ -112,91 +95,119 @@ document.addEventListener('DOMContentLoaded', () => {
 			checkBoxChangeColor();
 			priceAdjustment(100);
 		}
-
+		function enableBox (i, price) {
+			let conflictingCheckbox = checkbox[i];
+			priceAdjustment(price);
+			checkBoxChangeColor();
+			checkboxChecked ? conflictingCheckbox.parentNode.style.color = 'red' : conflictingCheckbox.parentNode.style.color = '';
+			checkboxChecked ? conflictingCheckbox.disabled = true : conflictingCheckbox.disabled = false;
+		}
+		function checkBoxChangeColor () {
+			checkboxChecked ? parentLabelNode.style.color = 'green' : parentLabelNode.style.color = '#000';
+		}
+		function priceAdjustment (price) {
+			return checkboxChecked ? activitiesCostTotal += price : activitiesCostTotal -= price;
+		}
 		activitiesCostTotalElement.innerHTML = 'Total: $';
 		activitiesCostTotalElement.innerHTML += activitiesCostTotal.toString();
-	});
+	}
 
-	paymentMethod.addEventListener("change", (e) => {
+	function changePaymentMethod (e) {
 		let selected = e.target.value;
 
 		selected == "credit card" ? $creditCardPayment.show() : $creditCardPayment.hide();
 		selected == "paypal" ? $paypalMessage.show() : $paypalMessage.hide();
 		selected == "bitcoin" ? $bitcoinMessage.show() : $bitcoinMessage.hide();
-	});
-
-	let letters = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	let isNotValidEmail = (node) => !letters.test(node.value) ? true : false;
-
-	let setNodeBorderRed = (node) => {
-		return node.style['box-shadow'] = '0 0 5px 2px red';
 	}
 
-	email.addEventListener("keyup", (e) => {
+	function emailLiveValidCheck () {
 		isNotValidEmail(email) ? setNodeBorderRed(email) : email.style['box-shadow'] = '';
-	});
+	}
 
-	submitButton.addEventListener('click', (e) => {
-		//e.preventDefault();
-		let msg = '';
-		let activitiesInput = false;
-		let activityLegend = activities.children[0];
-		let numbers = /^[0-9]+$/;
-		let errorAlert = '';
+	let letters = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	let formIsValid = false;
+	let msg = '';
+	let activitiesInput = false;
+	let activityLegend = activities.children[0];
+	let numbers = /^[0-9]+$/;
+	let errorAlert = '';
 
-		//func to build error message, called when nodes do not meet validation
-		let errorMessage = (msg) => {
-			if(errorAlert.length != 0){
-				errorAlert += ', ';
-			}
-			errorAlert += msg;
+	function errorMessage (msg) {
+		if(errorAlert.length != 0){
+			errorAlert += ', ';
 		}
+		errorAlert += msg;}
 
-		let isNotValidNumber = (node) => {
-				return !node.value.match(numbers);
-			}
+	function setNodeBorderRed (node) {
+		node.style['box-shadow'] = '0 0 5px 2px red';
+		return false;}
 
-		let isValid = (x, condition) => {
-			(isNotValidNumber(x) || condition) ? setNodeBorderRed(x) : x.style['box-shadow'] = '';
-		}
-
-		//beginning of input checks
-		if(isNotValidEmail(email)){
-			errorMessage("Email is not valid");
-			setNodeBorderRed(email);
-		}else{
-			email.style['box-shadow'] = '';
-		}
-
+	function isNameValid () {
 		if(!name.value.length > 0){
 			errorMessage("A name has not been entered");
 			setNodeBorderRed(name);
+			return false;
 		}else {
 			name.style['box-shadow'] = '';
-		}
+			return true;}}
 
+	function isNotValidEmail (node) { return !letters.test(node.value) }
+
+	function isEmailValid () {
+		if(isNotValidEmail(email)){
+			errorMessage("Email is not valid");
+			setNodeBorderRed(email);
+			return false;
+		}else{
+			email.style['box-shadow'] = '';
+			return true;
+		}}
+
+	function isValidNumber (node) { return node.value.match(numbers); }
+
+	function isValid (x, condition) {
+		if(isValidNumber(x) && !condition){
+			x.style['box-shadow'] = '';
+			return true;
+		}else{
+			x.style['box-shadow'] = '0 0 5px 2px red';
+			return false;
+		}}
+
+	function isPaymentMethodValid () {
 		if (paymentMethod.value == 'credit card') {
-			isValid(cc_num, (cc_num.value.length < 13 || cc_num.value.length > 16));
-			isValid(zip, zip.value.length != 5);
-			isValid(cvv, cvv.value.length != 3);
-		}
+			let boolCcNum = isValid(cc_num, (cc_num.value.length < 13 || cc_num.value.length > 16));
+			let boolZip = isValid(zip, zip.value.length != 5);
+			let boolCvv = isValid(cvv, cvv.value.length != 3);
 
-	 	for (let i = 0; i < activityInputs.length; i++) {
+			return (boolCcNum && boolZip && boolCvv);
+		}else {
+			return true;}}
+
+	function hasAnActivityBeenSelected () {
+		for (let i = 0; i < activityInputs.length; i++) {
 			if(activityInputs[i].checked){
 				activitiesInput = true;
-				i = activityInputs.length-1;
-			}
-		}
+				i = activityInputs.length-1;}}
 
 		!activitiesInput ? activityLegend.style.color = 'red' : activityLegend.style.color = '#184f68';
+		return activitiesInput;
+	}
 
-		//ended error message
-		if(errorAlert != ''){
-			alert(errorAlert += '.');
+	function checkIfValidForm(e) {
+		errorAlert = '';
+
+		let boolIsNameValid = isNameValid();
+		let boolIsEmailValid = isEmailValid();
+		let boolhasAnActivityBeenSelected = hasAnActivityBeenSelected();
+		let boolIsPaymentMethodValid = isPaymentMethodValid();
+
+		if( boolIsNameValid && boolIsEmailValid && boolhasAnActivityBeenSelected && boolIsPaymentMethodValid ){
+			return alert('Form is valid, information sent');
+		}else{
+			e.preventDefault();
+			if(errorAlert != ''){
+				return alert(errorAlert += '.');
+			}
 		}
-
-		//returns view back to the very top of the doucment
-		scroll(0,0);
-	});
-});
+		scroll(0,0);}});
